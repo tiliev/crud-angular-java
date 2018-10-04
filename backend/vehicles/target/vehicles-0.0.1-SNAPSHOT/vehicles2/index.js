d6a4bc80181cd1f85a0ba27226573ca9395af993
INFRAGISTICS\tiliev <tiliev@infragistics.com>
2018-10-04 09:56:38 +0300

    no message

--- backend/vehicles/target/vehicles-0.0.1-SNAPSHOT/vehicles2/index.js
+++
@@ -1,7 +0,0 @@
-angular.module('vehicles', [])
-.controller('VehiclesController', function($scope, $http) {
-    $http.get('/vehicles/getAllVehicles2').
-        then(function(response) {
-            $scope.vehicles = response.data;
-        });
-});
